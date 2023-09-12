1a6feaf360f0abb2eb562520d88df6757138909d
Jaster <julian.zecher@feuerregen.at>
2023-09-12 16:57:33 +0200

    now the camera does not shake anymore

---
+++ CheatMenu/DisableFallingShake.cs
@@ -0,0 +1,13 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+
+namespace CheatMenu;
+
+[HarmonyPatch(typeof(camFollowHead), "startFallingShake")]
+public class DisableFallingShake
+{
+    public static bool Prefix()
+    {
+        return !CheatMenu.IsGodModeEnabled;
+    }
+}
